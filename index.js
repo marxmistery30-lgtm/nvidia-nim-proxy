85252df8bfd3c280a92e28ecc7b7ca0f7edcc8bb
marxmistery30-lgtm <marxmistery30@gmail.com>
2026-09-08 12:35:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post(['/v1/chat/completions', '/chat/completions', '/v1', '/'], async (req, 
       stream 
     } = req.body;
 
-    const finalModel = model || 'deepseek-ai/deepseek-v3.1';
+    const finalModel = model || 'deepseek-ai/deepseek-v4-pro-0813';
     const finalTemp = temperature || 0.9;
     const finalMaxTokens = max_tokens ? Math.max(max_tokens, 8192) : 8192; // Reducido de 16384
     const finalStream = stream || false;
@@ -199,7 +199,7 @@ app.get('/v1/models', (req, res) => {
     object: 'list',
     data: [
       { 
-        id: 'deepseek-ai/deepseek-v3.1', 
+        id: 'deepseek-ai/deepseek-v4-pro-0813', 
         object: 'model', 
         created: 1234567890,
         owned_by: 'deepseek-ai' 
